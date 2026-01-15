c8b114287705cc08fd6116b11024d53033a79d1e
pmichelbrink <pmichelbrink@gmail.com>
2026-01-14 21:15:47 -0700

    Skip a couple more bad domains

--- AIToady.Harvester/ViewModels/BaseViewModel.cs
+++ AIToady.Harvester/ViewModels/BaseViewModel.cs
@@ -44,7 +44,8 @@ namespace AIToady.Harvester.ViewModels
             "fearlessmen.com", "allbackgrounds.com", "freeimagehosting.net",
             "novarata.net", "combatmachine.net", "hostingpics.net", "gunbroker.com", 
             "funnywebsite.com", "weaponarts.com", "bing.net", "sightpusher.com",
-            "groundedparents.com", "adrenaljunkie.com", "kgcoatings.com", "arco-iris.com"
+            "groundedparents.com", "adrenaljunkie.com", "kgcoatings.com", "arco-iris.com",
+            "picyard.com", "nodakspud.com"
         };
         protected Random _random = new Random();
         protected int _forumPageNumber = 1;
