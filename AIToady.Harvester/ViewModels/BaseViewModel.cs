010fe10c63e155cf18e4e8c4828e798d94ea227b
pmichelbrink <pmichelbrink@gmail.com>
2026-01-26 18:48:01 -0700

    Add logging of Image errors

--- AIToady.Harvester/ViewModels/BaseViewModel.cs
+++ AIToady.Harvester/ViewModels/BaseViewModel.cs
@@ -746,6 +746,7 @@ namespace AIToady.Harvester.ViewModels
                         }
                         else
                         {
+                            AddLogEntry($"Image failed: {result} - {imageUrl}");
                             //await _emailService.SendEmailAsync(Environment.MachineName + "@AIToady.com", "Image Error on " + Environment.MachineName, "Body");
                         }
                     }
