846b2feaffe798cb6b3e9f1670e85b248107908a
pmichelbrink <pmichelbrink@gmail.com>
2026-02-04 18:45:42 -0700

    Bad URL

--- AIToady.Harvester/ViewModels/BaseViewModel.cs
+++ AIToady.Harvester/ViewModels/BaseViewModel.cs
@@ -47,7 +47,8 @@ namespace AIToady.Harvester.ViewModels
             "groundedparents.com", "adrenaljunkie.com", "kgcoatings.com", "arco-iris.com",
             "picyard.com", "nodakspud.com", "vcmedia.vn", "villagephotos.com", "geocities.com",
             "gunscience.com", "picfury.com", "handgunblog.com", "htmlsitedesign.com", "blackwellindustries.com",
-            "62x54r.net", "zombieboxes.com", "hunt101.com", "nothingbutguns.com", "360WD.com", "photos.smugmug.com"
+            "62x54r.net", "zombieboxes.com", "hunt101.com", "nothingbutguns.com", "360WD.com", "photos.smugmug.com",
+            "community.webshots.com"
         };
         protected Random _random = new Random();
         protected int _forumPageNumber = 1;
